2cfb76702a7a7bf3116a69ea0a3776e46454f589
unknown <s27345@pjwstk.edu.pl>
2025-04-02 20:10:29 +0200

    #17 SQL: SELECT * FROM Emp WHERE Comm > 400;

--- Tutorial4/Tutorial4Tests/AdvancedEmpDeptTests.cs
+++ Tutorial4/Tutorial4Tests/AdvancedEmpDeptTests.cs
@@ -83,9 +83,9 @@ public class AdvancedEmpDeptTests
     {
         var emps = Database.GetEmps();
 
-        // var result = null; 
-        //
-        // Assert.True(result);
+        var result = emps.Exists(emp => emp.Comm > 400); 
+        
+        Assert.True(result);
     }
 
     // 18. Self-join to get employee-manager pairs
